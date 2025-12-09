7ed5230a287d0c3d9254a083321197a954206cd6
Yechang618 <yechang618@gmail.com>
2025-12-09 16:01:23 +0800

    update

--- rename_timestamp_column.py
+++ rename_timestamp_column.py
@@ -4,9 +4,9 @@ import pandas as pd
 # input_path = "datasets/kronos/BNBUSDT_kronos.csv"
 # input_path = "datasets/kronos/SOLUSDT_kronos.csv"
 # input_path = "batch/data/BNBUSDT_kronos.csv"
-input_path = "batch/data/KAITOUSDT_kronos.csv"
+# input_path = "batch/data/KAITOUSDT_kronos.csv"
 # input_path = "batch/data/DOTUSDT_kronos.csv"
-# input_path = "batch/data/ZECUSDT_kronos.csv"
+input_path = "batch/data/ZECUSDT_kronos.csv"
 # input_path = "batch/data/SOLUSDT_kronos.csv"
 df = pd.read_csv(input_path)
 
