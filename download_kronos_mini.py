d14787e93d0d9753332494d3ff38a0101d1b2134
Yechang618 <yechang618@gmail.com>
2025-12-16 21:14:57 +0800

    update

--- download_kronos_mini.py
+++ download_kronos_mini.py
@@ -5,15 +5,16 @@ from huggingface_hub import snapshot_download
 snapshot_download(
     repo_id="NeoQuasar/Kronos-Tokenizer-2k",
     # local_dir="pretrained/Kronos-Tokenizer-2k",
-    local_dir="batch/model/Kronos-Tokenizer-2k",    
+    # local_dir="batch/model/Kronos-Tokenizer-2k",    
+    local_dir="trained/sol_1min_10s/tokenizer/best_model",   
     local_dir_use_symlinks=False  # 避免 Windows symlink 问题
 )
 
 snapshot_download(
     repo_id="NeoQuasar/Kronos-mini",
     # local_dir="pretrained/Kronos-mini",
-    local_dir="batch/model/Kronos-mini",    
+    local_dir="trained/sol_1min_10s/basemodel/best_model",    
     local_dir_use_symlinks=False
 )
 
-print("✅ Kronos-mini and Tokenizer downloaded to ./batch/model/")
+print("✅ Kronos-mini and Tokenizer downloaded to ./trained/sol_1min_10s/basemodel/best_model and ./trained/sol_1min_10s/tokenizer/best_model/")
